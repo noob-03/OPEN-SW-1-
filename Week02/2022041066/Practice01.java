de0a3c8d828ff9d261b9066d7eb0a2bd1d32f029
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-09-11 18:28:54 +0900

    Update Practice01.java

--- Week02/2022041066/Practice01.java
+++ Week02/2022041066/Practice01.java
@@ -1,1 +1,14 @@
+//상수 PI를 선언하고 원의면적을 구하는 프로그램
 
+public class CircleArea {
+    public static void main(String[] args) {
+        final double PI =3.14; //원주율을 상수로 선언
+
+        double radius = 10.0; //원의 반지름 
+        double CircleArea = radius*radius*PI; //원의 면적 계산
+
+        System.out.println("원의 면적 =" + CircleArea);
+    }
+}
+
+// 결과값: 원의 면적 =314.0 
