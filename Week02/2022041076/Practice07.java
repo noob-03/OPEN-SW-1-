4f80342eee4fe30a8235a6deb139933477c871d6
김태효 <51405291+coomind@users.noreply.github.com>
2025-09-11 11:05:41 +0900

    Update and rename Practice7.java to Practice07.java

--- Week02/2022041076/Practice07.java
+++ Week02/2022041076/Practice07.java
@@ -1,9 +1,10 @@
 package Sample;
 
-public class Practice7{
+public class Practice07{
 	public static void main(String[] args) {
 		int a = 3, b=5;
 		
 		System.out.println("두 수의 차는"+((a>b)?(a-b):(b-a)));
 	}
+
 }
