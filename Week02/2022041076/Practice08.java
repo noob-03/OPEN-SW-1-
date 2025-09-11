ef58e1b2dc5a1998aa445072118e4dbb8952ec21
김태효 <51405291+coomind@users.noreply.github.com>
2025-09-11 11:05:56 +0900

    Update and rename Practice8.java to Practice08.java

--- Week02/2022041076/Practice08.java
+++ Week02/2022041076/Practice08.java
@@ -2,7 +2,7 @@ package Sample;
 
 import java.util.Scanner;
 
-public class Practice8{
+public class Practice08{
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("점수를 입력하시오:");
@@ -12,4 +12,5 @@ public class Practice8{
 		
 		scanner.close();
 	}
+
 }
