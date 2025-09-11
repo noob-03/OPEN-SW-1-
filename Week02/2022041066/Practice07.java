b34a5568881e4e180e3d6539a44776d5d7a9a75d
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-09-11 18:26:19 +0900

    Update Practice07.java

--- Week02/2022041066/Practice07.java
+++ Week02/2022041066/Practice07.java
@@ -1,1 +1,12 @@
+//조건연산자의사례중 하나이다. 실행결과는 무엇인가?
+
+public class TernaryOperator{
+    public static void main (String[] args) {
+        int a = 3, b = 5;
+        
+        System.out.println("두수의차는" + ((a>b)?(a-b):(b-a)));
+    }
+}
+
+//출력값 : 두수의차는2
 
