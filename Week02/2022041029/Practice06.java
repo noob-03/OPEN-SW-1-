9ceaefe6a36688c8c66d8e0c98712eda0a6eda13
SanghyeokKo <118700881+SanghyeokKo@users.noreply.github.com>
2025-09-11 11:21:24 +0900

    Create Practice06.java

---
+++ Week02/2022041029/Practice06.java
@@ -0,0 +1,17 @@
+public class LogicalOperator{
+	public static void main (String[] args) {
+		// 비교연산
+		System.out.println('a' > 'b');
+		System.out.println(3 >= 2);
+		System.out.println(-1 < 0);
+		System.out.println(3.45 <= 2);
+		System.out.println(3 == 2);
+		System.out.println(3 != 2);
+		System.out.println(!(3 != 2));
+		
+		// 비교연산과논리연산복합
+		System.out.println((3 > 2) && (3 > 4));
+		System.out.println((3 != 2) || (-1 > 0));
+		System.out.println((3 != 2) ^ (-1 > 0));
+		}
+}
