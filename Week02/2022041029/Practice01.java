67ffa569287d2265785b3e0ef0be0b31f7a952d3
SanghyeokKo <118700881+SanghyeokKo@users.noreply.github.com>
2025-09-11 10:53:32 +0900

    Create Practice01.java

---
+++ Week02/2022041029/Practice01.java
@@ -0,0 +1,9 @@
+public class CircleArea{
+    public static void main(String[] args) {
+        final double PI = 3.14; // 원주율을상수로선언
+        double radius = 10.0; // 원의반지름
+        double circleArea= radius*radius*PI; // 원의면적계산
+        // 원의면적을화면에출력한다.
+        System.out.println("원의면적= " + circleArea);
+    }
+ }
