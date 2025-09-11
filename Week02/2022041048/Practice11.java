9f61995516dbf6b834f2154c60f231c939070b7a
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-09-11 20:23:53 +0900

    Update Practice11.java

--- Week02/2022041048/Practice11.java
+++ Week02/2022041048/Practice11.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Hello2025 {
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		System.out.print("점수를입력하세요(0~100): ");
+		
+		int score = scanner.nextInt();
+		
+		System.out.print("학년을입력하세요(1~4): ");
+		
+		int year = scanner.nextInt();
+		
+		if(score >= 60) { 
+			if(year != 4)
+				System.out.println("합격!"); 
+			else if(score >= 70)
+				System.out.println("합격!"); 
+			else
+				System.out.println("불합격!"); 
+		}
+		else 
+			System.out.println("불합격!");
+		scanner.close();
+	}
+}
