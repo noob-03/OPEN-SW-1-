999f66c60040e5b987024aa5faf0c642b5a46775
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-09-11 20:22:30 +0900

    Update Practice10.java

--- Week02/2022041048/Practice10.java
+++ Week02/2022041048/Practice10.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Hello2025 {
+	public static void main(String[] args) {
+		char grade;
+		Scanner scanner= new Scanner(System.in);
+		System.out.print("점수를입력하세요(0~100): ");
+		
+		int score = scanner.nextInt();
+		
+		if(score >= 90) 
+			grade = 'A';
+		else if(score >= 80)
+			grade = 'B';
+		else if(score >= 70)
+			grade = 'C';
+		else if(score >= 60)
+			grade = 'D';
+		else 
+			grade = 'F';
+		
+		System.out.println("학점은"+ grade + "입니다.");
+		scanner.close();
+	}
+}
