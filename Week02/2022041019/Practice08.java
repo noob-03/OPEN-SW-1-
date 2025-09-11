b2fde33dcd36abe31208bb4c54fc93ea73eedecc
Kim JinSIk <99391603+Sweettreee@users.noreply.github.com>
2025-09-11 11:01:27 +0900

    Create Practice08.java

---
+++ Week02/2022041019/Practice08.java
@@ -0,0 +1,16 @@
+package AssignmentDay1;
+import java.util.Scanner;
+
+public class SuccessOrFail {
+
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		
+		System.out.print("점수를입력하시오: ");
+		int score = scanner.nextInt();
+		if (score >= 80)
+			System.out.println("축하합니다! 합격입니다.");
+		scanner.close();
+	}
+
+}
