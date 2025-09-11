8b46e2e62df578de7980f9d6b1c93b72b655dc30
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-09-11 18:25:55 +0900

    Update Practice08.java

--- Week02/2022041066/Practice08.java
+++ Week02/2022041066/Practice08.java
@@ -1,1 +1,20 @@
+//시험점수가 80점이상이라면 합격판별을 하는 프로그램을 작성하라
 
+import java.util.Scanner;
+
+public class SuccessOrFail{
+    public static void main (String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.print("점수를 입력하시오: ");
+        int score = scanner.nextInt();
+        if (score >= 80)
+            System.out.println("축하합니다! 합격입니다.");
+     
+            scanner.close();    
+    }
+}           
+/*  출력값
+점수를 입력하시오: 80
+축하합니다! 합격입니다. 
+*/
