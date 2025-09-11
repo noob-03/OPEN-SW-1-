22f4e75f68ffd6d0ed98602c5492250be0dfab06
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-09-11 20:16:19 +0900

    Update Practice08.java

--- Week02/2022041048/Practice08.java
+++ Week02/2022041048/Practice08.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
 
+public class Hello2025 {
+
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		System.out.print("점수를입력하시오: ");
+		int score = scanner.nextInt();
+		if (score >= 80)
+		System.out.println("축하합니다! 합격입니다.");
+		scanner.close();
+	}
+}
