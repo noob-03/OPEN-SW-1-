fc72372087a6fc6bd9548c052e50b03fcb374ba5
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-09-11 11:14:03 +0900

    Update Practice14.java

--- Week02/2022041048/Practice14.java
+++ Week02/2022041048/Practice14.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
 
+public class wwe1104 {
+  public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("가위바위보 게임입니다. 가위, 바위, 보 중에서 입력하세요");
+		
+		System.out.print("철수 >>");
+		String name1 = scanner.next();
+		
+		System.out.print("영희 >>");
+		String name2 = scanner.next();
+		
+		String var="";
+		if (name1.equals("보") && name2.equals("가위")) 
+            var = "영희";
+        else if (name1.equals("가위") && name2.equals("바위")) 
+            var = "영희";
+        else if (name1.equals("바위") && name2.equals("보")) 
+            var = "영희";
+        else if (name1.equals("가위") && name2.equals("보")) 
+            var = "철수";
+        else if (name1.equals("바위") && name2.equals("가위")) 
+            var = "철수";
+        else if (name1.equals("보") && name2.equals("바위")) 
+            var = "철수";
+        else if (name1.equaㅁls(name2)) 
+            var = "비김";
+		System.out.print(var+"가 이겼습니다");
+		
+		scanner.close();
+}
+}
