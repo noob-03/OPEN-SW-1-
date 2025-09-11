c10fa6f5564f77a8cd678c4e6b339d2d192780b3
Kim JinSIk <99391603+Sweettreee@users.noreply.github.com>
2025-09-11 10:57:40 +0900

    Update Practice01.java

--- Week02/2022041019/Practice01.java
+++ Week02/2022041019/Practice01.java
@@ -1,19 +1,14 @@
 package AssignmentDay1;
 
-public class TypeConversion {
+public class circleArea {
 
 	public static void main(String[] args) {
-		byte b = 127;
-		int i = 100;
-		
-		System.out.println(b+i);
-		System.out.println(10/4);
-		System.out.println(10.0/4);
-		System.out.println((char)0x12340041);
-		System.out.println((byte)(b+i));
-		System.out.println((int)2.9 + 1.8);
-		System.out.println((int)(2.9 + 1.8));
-		System.out.println((int)2.9 + (int)1.8);
+		final double PI = 3.14;
+
+        double radius = 10.0;
+        double circleArea = radius*radius*PI;
+
+        System.out.println("원의 면적 = " + circleArea);
 	}
 
 }
