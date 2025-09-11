470ab106d22856b4aca07ae844c619fb0b3214c4
Kim JinSIk <99391603+Sweettreee@users.noreply.github.com>
2025-09-11 10:56:47 +0900

    Update Practice01.java

--- Week02/2022041019/Practice01.java
+++ Week02/2022041019/Practice01.java
@@ -1,11 +1,19 @@
 package AssignmentDay1;
 
-public class TernaryOperator {
+public class TypeConversion {
 
 	public static void main(String[] args) {
-		int a=3, b=5;
+		byte b = 127;
+		int i = 100;
 		
-		System.out.println("두 수의 차는" + ((a>b)?(a-b):(b-a)));
+		System.out.println(b+i);
+		System.out.println(10/4);
+		System.out.println(10.0/4);
+		System.out.println((char)0x12340041);
+		System.out.println((byte)(b+i));
+		System.out.println((int)2.9 + 1.8);
+		System.out.println((int)(2.9 + 1.8));
+		System.out.println((int)2.9 + (int)1.8);
 	}
 
 }
