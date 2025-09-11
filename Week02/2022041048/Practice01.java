df4eea1834245c774493c8014259c3b7d2e4eff6
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-09-11 20:09:24 +0900

    Update Practice01.java

--- Week02/2022041048/Practice01.java
+++ Week02/2022041048/Practice01.java
@@ -1,1 +1,12 @@
+public class Hello2025 {
 
+	public static void main(String[] args) {
+		final double Pl = 3.14;
+		
+		double radius = 10.0;
+		double circleArea = radius*radius*Pl;
+		
+		System.out.println("원의 면적 = "+circleArea);
+	}
+
+}
