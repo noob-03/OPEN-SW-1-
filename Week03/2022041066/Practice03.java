e735f12d92412622ad06e8dafdccd0628df84a49
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-09-18 12:02:15 +0900

    Update Practice03.java

--- Week03/2022041066/Practice03.java
+++ Week03/2022041066/Practice03.java
@@ -1,1 +1,13 @@
+// ‘a’ 부터‘z’ 까지출력하는프로그램을작성
+public class DoWhileSample{
+    public static void main (String[] args) {
+        char c = 'a';
+        
+        do {
+            System.out.print(c);
+            c = (char) (c + 1);
+        } while (c <= 'z');
+    }
+} 
 
+//출력값: abcdefghijklmnopqrstuvwxyz
