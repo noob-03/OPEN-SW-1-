c35242fe5d1696eef31ac83134d581eb728d3f94
SanghyeokKo <118700881+SanghyeokKo@users.noreply.github.com>
2025-09-18 11:17:01 +0900

    Create Practice15.java

---
+++ Week03/2022041029/Practice15.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DevideByZeroHandling{
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		while(true) {
+			System.out.print("나뉨수를입력하시오:"); 
+			int dividend = scanner.nextInt(); // 나뉨수입력
+			System.out.print("나눗수를입력하시오:"); 
+			int divisor = scanner.nextInt(); // 나눗수입력
+			try {
+				System.out.println(dividend + "를"+ divisor + "로나누면몫은"+ dividend/divisor + "입니다.");
+				break; // 정상적인나누기완료후 while 벗어나기
+			}
+			catch(ArithmeticException e) { // ArithmeticException예외처리코드
+				System.out.println("0으로나눌수없습니다. 다시입력하세요");
+			}
+		}
+		scanner.close();
+	}
+}
