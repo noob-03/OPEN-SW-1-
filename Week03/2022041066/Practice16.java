18f933cf009b09c1ff626d4edc4be1b48f9a103d
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-09-18 12:09:53 +0900

    Update Practice16.java

--- Week03/2022041066/Practice16.java
+++ Week03/2022041066/Practice16.java
@@ -1,1 +1,23 @@
+public class ArrayException{
+    public static void main (String[] args) {
+        int[] intArray= new int[5];
+        intArray[0] = 0;
+        try {
+            for (int i=0; i<5; i++) {
+                intArray[i+1] = i+1 + intArray[i];
+                System.out.println("intArray["+i+"]"+"="+intArray[i]);
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("배열의인덱스가범위를벗어났습니다.");
+        }
+    }
+}
 
+/* 출력 결과 예시
+intArray[0]=0
+intArray[1]=1
+intArray[2]=3
+intArray[3]=6
+배열의인덱스가범위를벗어났습니다.
+*/
