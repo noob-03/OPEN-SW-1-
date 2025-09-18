79a6d0777b8323095decc2d1080c6e0f59edcb31
SanghyeokKo <118700881+SanghyeokKo@users.noreply.github.com>
2025-09-18 11:03:14 +0900

    Create Practice06.java

---
+++ Week03/2022041029/Practice06.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+	public class BreakExample{
+		public static void main(String[] args) {
+			Scanner scanner= new Scanner(System.in);
+			System.out.println("exit을입력하면종료합니다.");
+			while(true) {
+				System.out.print(">>");
+				String text = scanner.nextLine();
+				if(text.equals("exit")) // "exit"이입력되면반복종료
+					break; // while 문을벗어남
+		}
+		System.out.println("종료합니다...");
+		scanner.close();
+	}
+}
