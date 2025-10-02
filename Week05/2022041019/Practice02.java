7565a1e6cbe1a25a35d1d1af0af729a3c6dacf59
Kim JinSIk <99391603+Sweettreee@users.noreply.github.com>
2025-10-02 17:42:13 +0900

    Create Practice02.java

---
+++ Week05/2022041019/Practice02.java
@@ -0,0 +1,21 @@
+package Assignment3;
+import java.util.Scanner;
+
+public class Rectangle {
+
+	int width;
+	int height;
+	public int getArea() {
+	 return width*height;
+	}
+	public static void main(String[] args) {
+		Rectangle rect = new Rectangle(); // 객체생성
+		Scanner scanner = new Scanner(System.in);
+		System.out.print(">> ");
+		rect.width= scanner.nextInt();
+		rect.height= scanner.nextInt();
+		System.out.println("사각형의면적은" + rect.getArea());
+		scanner.close();
+	}
+
+}
