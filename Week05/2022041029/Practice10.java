89e110e86cd6a949edd413ab44398604e8b752ee
SanghyeokKo <118700881+SanghyeokKo@users.noreply.github.com>
2025-10-02 16:14:00 +0900

    Create Practice10.java

---
+++ Week05/2022041029/Practice10.java
@@ -0,0 +1,14 @@
+public class Sample {
+	public int a;
+	private int b;
+	int c;
+}
+
+public class AccessEx{
+	public static void main(String[]args) {
+		Sample aClass = new Sample();
+		aClass.a = 10;
+		aClass.b = 10;	//private이기 때문에 컴파일 오류!!
+		aClass.c = 10;
+	}
+}
