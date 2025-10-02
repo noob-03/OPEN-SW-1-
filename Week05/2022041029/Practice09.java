5b16d85de7af055645ff462b9ac2119b5a7f8ca4
SanghyeokKo <118700881+SanghyeokKo@users.noreply.github.com>
2025-10-02 16:12:36 +0900

    Create Practice09.java

---
+++ Week05/2022041029/Practice09.java
@@ -0,0 +1,11 @@
+public class GarbageEx {
+	public static void main(String[]args) {
+		String a = new String("Good");
+		String b = new String("Bad");
+		String c = new String("Normal");
+		String d,e;
+		a = null;	//가비지 발생
+		d = c;
+		c = null;
+	}
+}
