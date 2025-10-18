0e47b63b97a72d6e9cb5a4769b7e54945a23a34c
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-18 19:49:38 +0900

    Update Practice08.java

--- Week06/2022041048/Practice08.java
+++ Week06/2022041048/Practice08.java
@@ -1,1 +1,37 @@
+package yoon_young_chan;
 
+interface PhoneInterface {
+    final int TIMEOUT = 10000;   
+    void sendCall();           
+    void receiveCall();         
+
+    default void printLogo() {  
+        System.out.println("** Phone **");
+    }
+}
+
+class SamsungPhone implements PhoneInterface {
+
+    public void sendCall() {
+        System.out.println("띠리리리링");
+    }
+
+    public void receiveCall() {
+        System.out.println("전화가 왔습니다.");
+    }
+
+    public void flash() {
+        System.out.println("전화기에 불이 켜졌습니다.");
+    }
+}
+
+public class Hello2025 {
+    public static void main(String[] args) {
+        SamsungPhone phone = new SamsungPhone();
+
+        phone.printLogo();    
+        phone.sendCall();      
+        phone.receiveCall();  
+        phone.flash();         
+    }
+}
