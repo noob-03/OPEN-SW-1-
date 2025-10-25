a449b1c44f2f199c94aec9716f0ef7ce0bf90316
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 21:40:36 +0900

    Add Hello2025 class to demonstrate Integer usage

--- Week07/2022041048/Practice-Module-06.java
+++ Week07/2022041048/Practice-Module-06.java
@@ -1,1 +1,11 @@
+package yoon_young_chan;
 
+public class Hello2025{
+	public static void main(String[] args) {
+		int n = 10;
+		Integer intObject= n; 
+		System.out.println("intObject= " + intObject);
+		int m = intObject+ 10; 
+		System.out.println("m = " + m);
+	}
+}
