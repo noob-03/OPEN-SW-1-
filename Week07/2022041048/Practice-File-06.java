abdf7b8ebfc115caf7d2ed802e81141d3339857f
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 22:09:52 +0900

    Update Practice-File-06.java

--- Week07/2022041048/Practice-File-06.java
+++ Week07/2022041048/Practice-File-06.java
@@ -1,1 +1,22 @@
+package yoon_young_chan;
 
+import java.io.*;
+public class Hello2025{
+	public static void main(String[] args) {
+		byte b[] = new byte [6]; 
+		try {
+			FileInputStream fin = new FileInputStream("c:\\Temp\\test.out");
+			int n=0, c;
+			while((c = fin.read())!= -1) {
+				b[n] = (byte)c;
+				n++;}
+			System.out.println("c:\\Temp\\test.out에서읽은배열을출력합니다.");
+			for(int i=0; i<b.length; i++) System.out.print(b[i] + " ");
+			System.out.println();
+			fin.close();
+		} 
+		catch(IOException e) {
+			System.out.println( "c:\\Temp\\test.out에서읽지못했습니다.경로명을체크해보세요");
+		}
+	}
+}
