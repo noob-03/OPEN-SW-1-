9c81d2aef8c5b5f7538ff7b3784d2b90dbb1c32a
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 21:54:46 +0900

    Update Practice-Generic-04.java

--- Week07/2022041048/Practice-Generic-04.java
+++ Week07/2022041048/Practice-Generic-04.java
@@ -1,1 +1,24 @@
+package yoon_young_chan;
 
+import java.util.*;
+public class Hello2025{
+	public static void main(String[] args) {
+		Vector<Integer> v = new Vector<Integer>();
+		v.add(5);
+		v.add(4);
+		v.add(-1);
+		v.add(2, 100); 
+		Iterator<Integer> it = v.iterator();
+		while(it.hasNext()) {
+			int n = it.next();
+			System.out.println(n);
+		}
+		int sum = 0;
+		it = v.iterator();
+		while(it.hasNext()) {
+			int n = it.next();
+			sum += n;
+		}
+		System.out.println("벡터에있는정수합: " + sum);
+	}
+}
