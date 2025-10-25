9b1bc4e9f743750f269582704d486b3622c87edd
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 21:44:02 +0900

    Update Practice-Module-08.java

--- Week07/2022041048/Practice-Module-08.java
+++ Week07/2022041048/Practice-Module-08.java
@@ -1,1 +1,23 @@
+package yoon_young_chan;
+
+public class Hello2025{
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("This");
+		sb.append(" is pencil"); 
+		System.out.println(sb);
+		sb.insert(7, " my"); 
+		System.out.println(sb);
+		sb.replace(8, 10, "your"); 
+		System.out.println(sb);
+		sb.delete(8, 13); 
+		System.out.println(sb);
+		sb.setLength(4); 
+		System.out.println(sb);
+	}
+}
+/*This is pencil
+This is my pencil
+This is your pencil
+This is pencil
+This*/
 
