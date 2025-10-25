6155f85c73cd99e2746f109134d5cc9ec1fe5903
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 21:44:51 +0900

    Update Practice-Module-09.java

--- Week07/2022041048/Practice-Module-09.java
+++ Week07/2022041048/Practice-Module-09.java
@@ -1,1 +1,11 @@
+package yoon_young_chan;
+
+import java.util.StringTokenizer;
+public class Hello2025{
+	public static void main(String[] args) {
+		StringTokenizer st= new StringTokenizer("홍길동/장화/홍련/콩쥐/팥쥐", "/");
+		while (st.hasMoreTokens())
+			System.out.println(st.nextToken());
+	}
+}
 
