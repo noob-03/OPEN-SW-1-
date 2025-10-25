00ddbbc74b66313cf253c7e107ddd25b0c559c49
SanghyeokKo <118700881+SanghyeokKo@users.noreply.github.com>
2025-10-25 15:46:41 +0900

    Create Practice-File-03.java

---
+++ Week07/2022041029/Practice-File-03.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class FileReadHangulFail {
+    public static void main(String[] args) {
+        InputStreamReader in = null;
+        FileInputStream fin = null;
+        try {
+            fin = new FileInputStream("c:\\hangul.txt");
+            in = new InputStreamReader(fin, "US-ASCII");
+            int c;
+            System.out.println("인코딩문자집합은" + in.getEncoding());
+            while ((c = in.read()) != -1) {
+                System.out.print((char) c);
+            }
+            in.close();
+            fin.close();
+        } catch (IOException e) {
+            System.out.println("입출력오류");
+        }
+    }
+}
