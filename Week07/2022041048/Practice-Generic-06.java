de08e64b8227d7d6d08b5e094b71a0fcbd5a626a
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 21:57:57 +0900

    Update Practice-Generic-06.java

--- Week07/2022041048/Practice-Generic-06.java
+++ Week07/2022041048/Practice-Generic-06.java
@@ -1,1 +1,21 @@
+package yoon_young_chan;
 
+import java.util.*;
+public class Hello2025{
+	public static void main(String[] args) {
+		HashMap<String, Integer> javaScore= new HashMap<String, Integer>();
+		javaScore.put("김성동", 97);
+		javaScore.put("황기태", 88);
+		javaScore.put("김남윤", 98);
+		javaScore.put("이재문", 70);
+		javaScore.put("한원선", 99);
+		System.out.println("HashMap의요소개수:" + javaScore.size());
+		Set<String> keys = javaScore.keySet();
+		Iterator<String> it = keys.iterator();
+		while(it.hasNext()) {
+			String name = it.next();
+			int score = javaScore.get(name);
+			System.out.println(name + " : " + score);
+		}
+	}
+}
