929302947aea4bd0e522b07b7571901099ca0192
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 21:50:35 +0900

    Update Practice-Generic-03.java

--- Week07/2022041048/Practice-Generic-03.java
+++ Week07/2022041048/Practice-Generic-03.java
@@ -1,1 +1,18 @@
+package yoon_young_chan;
 
+import java.util.*;
+public class Hello2025{
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<String>();
+		Scanner scanner= new Scanner(System.in);
+		for(int i=0; i<4; i++) {
+			System.out.print("이름을입력하세요>>");
+			String s = scanner.next();
+			a.add(s); 
+		}
+		for(int i=0; i<a.size(); i++) {
+			String name = a.get(i);
+			System.out.print(name + " ");
+		}
+	}
+}
