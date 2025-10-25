705a481d80917b763d0f86223889f550dc2bc805
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 22:10:29 +0900

    Update Practice-File-07.java

--- Week07/2022041048/Practice-File-07.java
+++ Week07/2022041048/Practice-File-07.java
@@ -1,1 +1,23 @@
+package yoon_young_chan;
 
+import java.io.*;
+import java.util.Scanner;
+public class Hello2025 {
+	public static void main(String[] args) {
+		FileReader fin = null;
+		int c;
+		try {
+			fin = new FileReader("c:\\Temp\\test2.txt");
+			BufferedOutputStream out = new
+					BufferedOutputStream(System.out, 5);
+			while ((c = fin.read()) != -1) {
+				out.write(c);
+			}
+			new Scanner(System.in).nextLine();
+			out.flush();
+			fin.close();
+			out.close();
+		} 
+		catch (IOException e) { e.printStackTrace(); }
+	}
+}
