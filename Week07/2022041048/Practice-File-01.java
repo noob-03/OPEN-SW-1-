be86038731d6f0d2e9a45a358fb5e08113a796d4
Yoon young chan <118726071+yoon1104@users.noreply.github.com>
2025-10-25 22:04:07 +0900

    Update Practice-File-01.java

--- Week07/2022041048/Practice-File-01.java
+++ Week07/2022041048/Practice-File-01.java
@@ -1,1 +1,20 @@
+package yoon_young_chan;
 
+import java.io.*;
+public class Hello2025{
+    public static void main(String[] args) {
+        FileReader fin = null;
+        try {
+            fin = new FileReader("c:\\windows\\system.ini");
+            int c;
+            while ((c = fin.read()) != -1) {
+                System.out.print((char)c);
+            }
+            fin.close();
+        }
+
+        catch (IOException e) {
+            System.out.println("입출력오류");
+        }
+    }
+}
