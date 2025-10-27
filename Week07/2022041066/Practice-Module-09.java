e540dd19d5262e42ec7782d972da111ff3114805
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-10-27 09:13:28 +0900

    Create Practice-Module-09.java

---
+++ Week07/2022041066/Practice-Module-09.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+import java.util.StringTokenizer;
+
+public class Practice09 {
+    public static void main(String[] args) {
+        StringTokenizer st = new StringTokenizer("홍길동/장화/홍련/콩쥐/팥쥐","/");
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+    }
+}
