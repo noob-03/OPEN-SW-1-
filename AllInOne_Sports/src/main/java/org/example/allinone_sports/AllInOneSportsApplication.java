91c05ca1eb87c0311c6e4b245c1415f2433b2931
Sweettreee <xhrlehgus1234@naver.com>
2025-11-06 17:38:12 +0900

    fist spring boot commit

---
+++ AllInOne_Sports/src/main/java/org/example/allinone_sports/AllInOneSportsApplication.java
@@ -0,0 +1,13 @@
+package org.example.allinone_sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInOneSportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllInOneSportsApplication.class, args);
+    }
+
+}
