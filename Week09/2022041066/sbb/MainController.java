23e5795bbd53ee16c2b2434d19ff29dfa4ce0fb8
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-11-07 22:36:13 +0900

    Update MainController.java

--- Week09/2022041066/sbb/MainController.java
+++ Week09/2022041066/sbb/MainController.java
@@ -1,1 +1,15 @@
 
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        // http://localhost:8080/ 접속 → /question/list 로 리다이렉트
+        return "redirect:/question/list";
+    }
+}
