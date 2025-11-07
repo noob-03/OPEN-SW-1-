be1db9a8627ade737633ac30a27db6aee27010c0
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-11-07 22:38:21 +0900

    Update AnswerController.java

--- Week09/2022041066/sbb/answer/AnswerController.java
+++ Week09/2022041066/sbb/answer/AnswerController.java
@@ -1,1 +1,29 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/answer")
+public class AnswerController {
+
+    private final AnswerService answerService;
+    private final QuestionService questionService;
+
+    // 답변 등록 처리: POST /answer/create/{id}
+    @PostMapping("/create/{id}")
+    public String create(@PathVariable("id") Integer id,
+                         @RequestParam String content) {
+
+        Question question = this.questionService.getQuestion(id);
+        this.answerService.create(question, content);
+
+        // 다시 해당 질문 상세 화면으로 이동
+        return String.format("redirect:/question/detail/%d", id);
+    }
+}
 
