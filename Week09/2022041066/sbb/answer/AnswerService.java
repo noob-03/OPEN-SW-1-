13991e41c9c8cb12f63ca4e764b2b23977be15ce
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-11-07 22:38:07 +0900

    Update AnswerService.java

--- Week09/2022041066/sbb/answer/AnswerService.java
+++ Week09/2022041066/sbb/answer/AnswerService.java
@@ -1,1 +1,24 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    // 답변 생성
+    public Answer create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setQuestion(question);
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        return this.answerRepository.save(answer);
+    }
+}
 
