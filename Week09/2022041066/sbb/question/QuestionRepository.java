497d14902ec0905381d67125719f6be998dae1b4
juneyoung <116141741+noob-03@users.noreply.github.com>
2025-11-07 22:37:07 +0900

    Update QuestionRepository.java

--- Week09/2022041066/sbb/question/QuestionRepository.java
+++ Week09/2022041066/sbb/question/QuestionRepository.java
@@ -1,1 +1,8 @@
+package com.mysite.sbb.question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    // 기본 CRUD는 JpaRepository에서 제공
+}
 
