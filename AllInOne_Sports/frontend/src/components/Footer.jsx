45f94b88ecde6fc8012a855e2e72d80ddfe24eec
sang <sang3141592@gmail.com>
2025-11-18 22:14:47 +0900

    feat(frontend): 메인 페이지 UI 구현 및 그라데이션 디자인 적용

---
+++ AllInOne_Sports/frontend/src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    // bg-dark, text-white: 어두운 배경, 흰색 글씨, p-5: 큰 패딩
+    <footer className="bg-dark text-white p-5">
+      <div className="container">
+        <div className="row">
+          {/* 푸터 왼쪽 */}
+          <div className="col-md-6">
+            <h5 className="fw-bold">ALL-IN_SPORTS</h5>
+            <p className="text-white-50 small">
+              CBNU Software<br />
+              Opensource Project
+            </p>
+          </div>
+          {/* 푸터 오른쪽 */}
+          <div className="col-md-6 text-md-end">
+            <h6 className="text-white-50">Quick Link's</h6>
+            <ul className="list-unstyled">
+              <li><a href="/calender" className="text-white text-decoration-none">Calender</a></li>
+              <li><a href="/community" className="text-white text-decoration-none">Community</a></li>
+              <li><a href="/ticket" className="text-white text-decoration-none">Ticket</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
