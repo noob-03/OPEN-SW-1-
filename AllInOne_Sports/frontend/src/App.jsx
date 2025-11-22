12db45b9e6a77cdf42e8423d3e3cef3189e86f39
Sweettreee <xhrlehgus1234@naver.com>
2025-11-22 17:15:37 +0900

    feat: 게시글 표시 관련 페이지 및 백엔드 서버로 요청 보내기 기능 추가

--- AllInOne_Sports/frontend/src/App.jsx
+++ AllInOne_Sports/frontend/src/App.jsx
@@ -5,14 +5,16 @@ import Footer from './components/Footer';
 import LoginPage from './pages/LoginPage';
 import CookiePage from "./pages/CookiePage";
 import JoinPage from "./pages/JoinPage";
-import UserPage from "./pages/UserPage"
+import UserPage from "./pages/UserPage";
+import ListBoard from "./pages/ListBoardPage";
+// import CreateBoard from './pages/CreateBoardPage';
 
 function App() {
   return (
     // [수정] BrowserRouter가 전체 애플리케이션을 감싸도록 최상단에 배치
     <BrowserRouter> 
       {/* 1. Header는 이제 absolute position으로 페이지 위에 떠있게 됩니다. */}
-      <Header />
+      {/* <Header /> */}
 
       {/* 2. Routes 내부에서 경로에 따라 페이지를 렌더링합니다. */}
       <Routes>
@@ -20,10 +22,12 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
         <Route path="/cookie" element={<CookiePage />} />
+        <Route path="/board" element={<ListBoard />} />
+        <Route path="/create-board" element={<CreateBoard />} />
       </Routes>
 
       {/* 3. Footer는 라우터 내부에 위치합니다. */}
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
