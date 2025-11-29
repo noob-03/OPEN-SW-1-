f2ea5c4ff64caa2fd7e83a105e4300d445a78519
김태효 <51405291+coomind@users.noreply.github.com>
2025-11-30 04:02:48 +0900

    Update TeamEntity.java

--- AllInOne_Sports/backend/src/main/java/org/example/allinone_sports/domain/team/entity/TeamEntity.java
+++ AllInOne_Sports/backend/src/main/java/org/example/allinone_sports/domain/team/entity/TeamEntity.java
@@ -13,27 +13,22 @@ public class TeamEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "teamid") // DB에는 teamid (전체 소문자)로 되어있음
+    @Column(name = "team_id")
     private Long teamId;
 
-    @Column(name = "sportId") // DB의 sportId 컬럼과 직접 매핑 (자동 변환 방지)
     private Integer sportId; // 1 = KBO, 2 = KLeague
-
-    @Column(name = "name")
+    private String teamCode;   // K01, K02, ...
     private String name;
-
-    @Column(name = "logoUrl") // 중요! JPA가 자동으로 logo_url로 찾는 것을 방지
     private String logoUrl;
-
-    @Column(name = "league")
     private String league;
-
-    @Column(name = "snsLink") // 중요! sns_link로 변환 방지
     private String snsLink;
-
-    @Column(name = "ticketLink") // 중요! ticket_link로 변환 방지
     private String ticketLink;
-
-    @Column(name = "teamLink") // 중요! team_link로 변환 방지
     private String teamLink;
+    private String teamStadium;
+    private Integer teamRank;
+    private Integer teamPlayed;
+    private Integer teamPoint;
+    private Integer teamWon;
+    private Integer teamLost;
+    private Integer teamDraw;
 }
