b2328a6876209fa9c15173180a84ba043116cbb3
김태효 <51405291+coomind@users.noreply.github.com>
2025-11-30 04:03:25 +0900

    Update TeamService.java

--- AllInOne_Sports/backend/src/main/java/org/example/allinone_sports/domain/team/service/TeamService.java
+++ AllInOne_Sports/backend/src/main/java/org/example/allinone_sports/domain/team/service/TeamService.java
@@ -1,9 +1,12 @@
 package org.example.allinone_sports.domain.team.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.allinone_sports.crawler.KLeagueTeamCrawler;
 import org.example.allinone_sports.domain.team.entity.TeamEntity;
 import org.example.allinone_sports.domain.team.repository.TeamRepository;
 import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
+
 
 import java.util.List;
 
@@ -12,6 +15,7 @@ import java.util.List;
 public class TeamService {
 
     private final TeamRepository repo;
+    private final KLeagueTeamCrawler crawler;
 
     public List<TeamEntity> getAllTeams() {
         return repo.findAll();
@@ -38,4 +42,9 @@ public class TeamService {
     public String getTeamLogoByName(String name) {
         return getTeamByName(name).getLogoUrl();
     }
+
+    public List<TeamEntity> getTeamsBySport(int sportId) {
+        return repo.findBySportId(sportId);
+    }
+
 }
